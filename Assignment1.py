2938c0ae2372b6e050b59972ef229b49e9cae3c3
fenniweg <fenjawegener@gmx.de>
2026-09-22 09:29:17 +0200

    bem_alg

--- Assignment1.py
+++ Assignment1.py
@@ -1,12 +1,15 @@
 # Assignment 1  
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+#import the BEM_algorithm function from bem.py
+from bem import BEM_algorithm
+
+
 # Question 1
 # Compute the highest obtainable power coefficient, Cp,max (λmax, θp,max).
 # Using BEM two methods: the polynomial method and the Madsen et al. method. 
 # Compare the results of the two methods by plotting the power coefficient, Cp, as a function of the tip speed ratio, λ, for a range of pitch angles, θp. 
-
-
-#import the BEM_algorithm function from bem.py
-from bem import BEM_algorithm
 
 #Define range of tip speed ratios (lambda) and pitch angles (theta_p) to test
 tip_speed_ratio = [5,6,7,8,9,10]
@@ -16,14 +19,26 @@ method = 'Polynomial'  # or 'Madsen' remov later and loop
 #Outer loop over methods, inner loop over tip speed ratios and pitch angles
 #Loop over all combinations of tip speed ratios and pitch angles to compute thrust, torque, and power using BEM_algorithm
 #for method in ['Polynomial','Madsen']:
+thrust_list = []
+torque_list = []
+power_list = []
+tip_speed_list = []
+theta_list = []
 for s in tip_speed_ratio:
         for theta in theta_p:
             print(f"Tip speed ratio: {s}, Pitch angle: {theta}")
             print(f"Method: {method}")
-            thrust, torque, Power = BEM_algorithm(s,theta,method)
+            thrust, torque,Power= BEM_algorithm(s,theta,method)
             print(f"Thrust: {thrust}, Torque: {torque}")
             print(f"Power: {Power}")
-        
+            #Save for plotting later
+            tip_speed_list.append(s)
+            theta_list.append(theta)
+            # power_list.append(Power)
+            thrust_list.append(thrust)
+            torque_list.append(torque)
+            
 
-#implement list to store the results of thrust, torque, and power for each combination of tip speed ratio and pitch angle
-#plot the results of power coefficient, Cp, as a function of tip speed ratio, λ, for each pitch angle, θp, for both methods on the same plot
+
+
+
--- bem.py
+++ bem.py
@@ -77,14 +77,30 @@ def bem (r, R, B,rho,V_0,omega,theta_p,beta,chord,C_l,C_d,method):
 
     return a, a_prime, p_t,p_n,F
 
+def double_interpolation(alpha,t_over_c):
+    C_l_thickness = np.zeros((6))
+    C_d_thickness = np.zeros((6))
+    
+    for k in range (6):
+        C_l_thickness[k] = np.interp(alpha,airfoil_data[k][:,0],airfoil_data[k][:,1])
+        C_d_thickness[k] = np.interp(alpha,airfoil_data[k][:,0],airfoil_data[k][:,2])
+
+    #Interpolate to actual thickness
+    thickness_profile = [100,60,48,36,30.1,24.1] #thickness profile of the 6 airfoils in percentage
+    #Use np.argsort to sort the thickness profile and corresponding C_l and C_d values
+    sort_indices = np.argsort(thickness_profile)
+    x_p_sorted = np.array(thickness_profile)[sort_indices]
+    y_p_sorted_C_l = np.array(C_l_thickness)[sort_indices]
+    y_p_sorted_C_d = np.array(C_d_thickness)[sort_indices]
+
+    C_l = np.interp(t_over_c,x_p_sorted,y_p_sorted_C_l)
+    C_d = np.interp(t_over_c,x_p_sorted,y_p_sorted_C_d)
+
+    return C_l, C_d
 
 def BEM_algorithm (s,theta_p,method):
-    # omega = s*V_0/R
-    # print(f"Omega: {omega}")
-    omega = 0.9 #rad/s
-    V_0 = 10
-    
-
+    omega = s*V_0/R
+    omega = 0.9
     #Load blade data
     r_list = blade_dat['r'].values
     chord_list = blade_dat['c'].values
@@ -93,83 +109,63 @@ def BEM_algorithm (s,theta_p,method):
 
     p_n_list = []
     p_t_list = []
-    mu = []
 
     #loop through each blade element
     for i in range(len(r_list)):
-        if i ==3: #just for testing, remove later
-            break 
         r = r_list[i]
         chord = chord_list[i]
         beta = beta_list[i]
         t_over_c = t_over_c_list[i]
-        mu.append(r/R)
 
-        print(f"Blade element {i}: r={r}, chord={chord}, beta={beta}, t/c={t_over_c}")
+        sigma = chord*n_blades/(2*np.pi*r) 
 
     #Initialze a and a_prime, convergence tolerance
 
         a = 0
         a_prime = 0
-        epsilon = 1e-7
-    
-    #Calculate flowangle
-        psi = np.arctan((1-a)*V_0/((1+a_prime)*omega*r))
-        print(f"Initial psi: {psi}")
+        epsilon = 1e-6
 
-    #Compute local angle of attack alpha
-        alpha = psi-(beta+theta_p)
-        print(f"Initial alpha: {alpha}")
-        
-
-        #Lookup C_l and C_d from airfoil data based on alpha with double interpolation
-        #Interpolate first the values for each of the  6 different airfoils based on alpha
-        C_l_thickness = np.zeros((6))
-        C_d_thickness = np.zeros((6))
-
-       
-        
-        for k in range (6):
-            C_l_thickness[k] = np.interp(alpha,airfoil_data[k][:,0],airfoil_data[k][:,1])
-            C_d_thickness[k] = np.interp(alpha,airfoil_data[k][:,0],airfoil_data[k][:,2])
-
-        
-        #Interpolate to actual thickness
-        thickness_profile = [100,60,48,36,30.1,24.1]
-
-        #Use np.argsort to sort the thickness profile and corresponding C_l and C_d values
-        sort_indices = np.argsort(thickness_profile)
-        x_p_sorted = np.array(thickness_profile)[sort_indices]
-        y_p_sorted_C_l = np.array(C_l_thickness)[sort_indices]
-        y_p_sorted_C_d = np.array(C_d_thickness)[sort_indices]
-
-        C_l = np.interp(t_over_c,x_p_sorted,y_p_sorted_C_l)
-        C_d = np.interp(t_over_c,x_p_sorted,y_p_sorted_C_d)
-        print(f"t/c: {t_over_c}, Interpolated C_l: {C_l}, C_d: {C_d}")
-        
-        C_n = C_l*np.cos(psi) + C_d*np.sin(psi)
-        C_t = C_l*np.sin(psi) - C_d*np.cos(psi)
-
-        sigma = chord*n_blades/(2*np.pi*r) 
-        C_T = (1-a)**2*C_n*sigma/(np.sin(psi)**2)
-        
-        F = 2/np.pi*np.arccos(np.exp(-n_blades/2*(R-r)/(r*np.sin(abs(psi)))))
-
-        i = 0
+        count = 0
 
         while True:
-            i = i+1
+            count = count+1
+            #Calculate flowangle
+            psi = np.arctan((1-a)*V_0/((1+a_prime)*omega*r))
+    
+            #Compute local angle of attack alpha
+            alpha = np.degrees(psi)-(beta+theta_p)
+    
+            #Lookup C_l and C_d from airfoil data based on alpha with double interpolation
+            C_l, C_d = double_interpolation(alpha,t_over_c)
 
-            if a < 0.33: #no correction for a_star applied
+            #Compute normal and tangential force coefficients
+            C_n = C_l*np.cos(psi) + C_d*np.sin(psi)
+            C_t = C_l*np.sin(psi) - C_d*np.cos(psi)
+    
+            
+            dC_T = (1-a)**2*C_n*sigma/(np.sin(psi)**2)
+    
+            # Prandtl Tip Loss Correction F 
+            if np.abs(np.sin(psi)) < 1e-6:
+                F = 1.0
+            else:
+                exp_arg = -(n_blades / 2.0) * (R - r) / (r * np.abs(np.sin(psi)))
+                F = (2.0 / np.pi) * np.arccos(np.exp(exp_arg))
+                F = max(F, 1e-4)  # Avoid division by zero
+
+            #Axial Induction Factor a and Tangential Induction Factor a_prime for chosen method
+
+            if a <= 0.33: #no correction for a_star applied
                 a_star = sigma*C_n/(4*F*np.sin(psi)**2)*(1-a)
             else: # a_star with correction
                 if method == 'Polynomial': #third order polynomial
-                    dC_T = (1-a)**2*C_n*sigma/(np.sin(psi)**2)
+                    # dC_T = (1-a)**2*C_n*sigma/(np.sin(psi)**2)
                     a_star = dC_T/(4*F*(1-0.25*(5-3*a)*a))
-                    
+                    a_star = dC_T / (4.0 * F * (1.0 - 0.25 * (5.0 - 3.0 * a) * a))
                 if method =='Madsen': #Madsen et al
-                    dC_T= (1-a)**2*C_n*sigma/(np.sin(psi)**2)
-                    a_star = 0.246*(dC_T/F)+0.0586*(dC_T/F)**2+0.083*(dC_T/F)**3
+                    CT_F = dC_T / F
+                    a_star = 0.246 * CT_F + 0.0586 * (CT_F**2) + 0.0883 * (CT_F**3)
+                    #a_star = 0.246*(dC_T/F)+0.0586*(dC_T/F)**2+0.0883*(dC_T/F)**3
 
             a_new = 0.1*a_star+(1-0.1)*a #Update a with relaxation factor 0.1
 
@@ -179,48 +175,46 @@ def BEM_algorithm (s,theta_p,method):
 
             #Check for convergence
             if abs(a_new - a) < epsilon and abs(a_prime_new - a_prime) < epsilon:
-                print(f"Converged after {i} iterations")
+                # print(f"Converged after {i} iterations")
                 break
             else:
                 a = a_new
                 a_prime = a_prime_new
 
-            if i > 1000:
-                print("Warning: BEM did not converge after 1000 iterations")
+            if count > 1000:
+                print(f"Warning: BEM did not converge after 1000 iterations in element {i}")
                 break
 
-            #Calculate psi, C_n, C_t, sigma, C_T, F for the next iteration or return
-            psi = np.arctan((1-a)*V_0/((1+a_prime)*omega*r))
-            C_n = C_l*np.cos(psi) + C_d*np.sin(psi)
-            C_t = C_l*np.sin(psi) - C_d*np.cos(psi)
+
+        #if converged, calculate p_n and p_t for this blade element
             
-            sigma = chord*n_blades/(2*np.pi*r) 
-            C_T = (1-a)**2*C_n*sigma/(np.sin(psi)**2)
-                
-            F = 2/np.pi*np.arccos(np.exp(-n_blades/2*(R-r)/(r*np.sin(abs(psi)))))
 
-            V_rel = V_0*(1-a)/np.sin(psi)
+        V_rel = np.sqrt((V_0 * (1.0 - a))**2 + (omega * r * (1.0 + a_prime))**2)
 
-            p_n = 0.5*rho*(V_rel)**2*chord*C_n
-            p_t = 0.5*rho*(V_rel)**2*chord*C_t
+        p_n = 0.5*rho*(V_rel)**2*chord*C_n
+        p_t = 0.5*rho*(V_rel)**2*chord*C_t
 
         #Save p_n and p_t for each blade element
         p_n_list.append(p_n)
         p_t_list.append(p_t)
+        
 
-        #Integrate over the blade elements to get total thrust and torque
-        # thrust = np.trapz(p_n_list,mu)
-        # torque = np.trapz(p_t_list,mu)
+    #Force last element to be zero to avoid numerical issues at the tip
+    p_n_list[-1] = 0.0
+    p_t_list[-1] = 0.0
+    # Integrate over the blade elements to get total thrust and torque
+    thrust = n_blades*np.trapz(p_n_list,r_list)
+    torque = np.trapz(r_list*p_t_list,r_list)
 
-        # P = torque*omega
+    P = torque*omega
 
 
-    return p_n_list,p_t_list
+    return thrust,torque,P
 
 V_0 = 10
 theta_p = 0
 s = 5
-method = 'Madsen'
+method = ''
 #run test of BEM_algorithm with these values
 p_n, p_t= BEM_algorithm(s,theta_p,method)
 
